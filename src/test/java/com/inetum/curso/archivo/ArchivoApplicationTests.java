2af54d32a0c5bbd51605bdfd05b7a450fe857343
Álvaro <59648688+Smaladaxx@users.noreply.github.com>
2024-10-02 11:40:59 +0200

    Add files via upload
    maven add

---
+++ src/test/java/com/inetum/curso/archivo/ArchivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inetum.curso.archivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
